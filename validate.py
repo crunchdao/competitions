20019e12843de9e61420032d2d7150936dce1499
Caceresenzo <caceresenzo1502@gmail.com>
2024-03-09 00:50:14 +0100

    feat(validate): contact the api

--- validate.py
+++ validate.py
@@ -1,9 +1,19 @@
 import json
 import os
 import typing
+import urllib.parse
 
 import click
 import jsonschema
+import requests
+
+QUICKSTARTER_JSON_NAME = "quickstarter.json"
+SCHEMA_PATH = "schema/quickstarter.json"
+
+
+def print_tab(input: str):
+    for line in str(input).splitlines():
+        print(f"\t{line}")
 
 
 def _validate(
@@ -21,99 +31,114 @@ def _validate(
             files = [entrypoint]
         else:
             files = quickstarter_json["files"]
-        
+
         if entrypoint not in files:
             raise ValueError("entrypoint not in files list")
-        
+
         parent = os.path.dirname(path)
         missing = []
         for file in files:
             file_path = os.path.join(parent, file)
             if not os.path.exists(file_path):
                 missing.append(file_path)
-        
+
         if len(missing):
             raise ValueError("some files are missing:\n" + "\n".join(missing))
 
         print(f"{path}: valid")
-        return True
+        return quickstarter_json
     except Exception as error:
         print(f"{path}: invalid")
         print()
-        for line in str(error).splitlines():
-            print(f"\t{line}")
+        print_tab(str(error))
         print()
 
-        return False
-
-@click.group()
-def cli():
-    pass
+        return None
 
 
-@cli.command()
-@click.option("--quickstarter-json-name", default="quickstarter.json")
-@click.option("--schema-path", default="schema/quickstarter.json")
-@click.argument("root", default="./competitions/")
-def competitions(
-    quickstarter_json_name: str,
-    schema_path: str,
-    root: str
+def _validate_directories(
+    schema: dict,
+    root: str,
+    items: typing.List[typing.Dict],
+    competition_name: typing.Optional[str] = None
 ):
     success = True
 
-    with open(schema_path) as fd:
-        schema = json.load(fd)
-
-    for competition_name in os.listdir(root):
-        competition_root = os.path.join(root, competition_name)
-        if not os.path.isdir(competition_root):
-            continue
-
-        for quickstarter_name in os.listdir(competition_root):
-            quickstarter_root = os.path.join(competition_root, quickstarter_name)
-            if not os.path.isdir(quickstarter_root):
-                continue
-
-            quickstarter_json_path = os.path.join(quickstarter_root, quickstarter_json_name)
-            if not os.path.exists(quickstarter_json_path):
-                continue
-
-            if not _validate(schema, quickstarter_json_path):
-                success = False
-
-    if not success:
-        exit(1)
-
-
-@cli.command()
-@click.option("--quickstarter-json-name", default="quickstarter.json")
-@click.option("--schema-path", default="schema/quickstarter.json")
-@click.argument("root", default="./generic/")
-def generic(
-    quickstarter_json_name: str,
-    schema_path: str,
-    root: str
-):
-    success = True
-
-    with open(schema_path) as fd:
-        schema = json.load(fd)
-
-    for quickstarter_name in os.listdir(root):
-        quickstarter_root = os.path.join(root, quickstarter_name)
+    for quickstarter_root_name in os.listdir(root):
+        quickstarter_root = os.path.join(root, quickstarter_root_name)
         if not os.path.isdir(quickstarter_root):
             continue
 
-        quickstarter_json_path = os.path.join(quickstarter_root, quickstarter_json_name)
+        quickstarter_json_path = os.path.join(quickstarter_root, QUICKSTARTER_JSON_NAME)
         if not os.path.exists(quickstarter_json_path):
             continue
 
-        if not _validate(schema, quickstarter_json_path):
+        quickstarter_json = _validate(schema, quickstarter_json_path)
+        if quickstarter_json is None:
             success = False
+        else:
+            items.append(quickstarter_json)
+            quickstarter_json["name"] = quickstarter_root_name
+            quickstarter_json["competitionName"] = competition_name
+
+    return success
+
+
+@click.command()
+@click.option("--competition-root", default="./competitions/")
+@click.option("--generic-root", default="./generic/")
+@click.option("--api-base-url", envvar="API_BASE_URL", default="https://api.hub.crunchdao.com")
+@click.option("--contact-api", default=False)
+@click.option("--api-key", envvar="CRUNCHDAO_API_KEY", default=None)
+def cli(
+    competition_root: str,
+    generic_root: str,
+    # ---
+    api_base_url: str,
+    contact_api: str,
+    api_key: str,
+):
+    success = True
+    items = []
+
+    with open(SCHEMA_PATH) as fd:
+        schema = json.load(fd)
+
+    for competition_name in os.listdir(competition_root):
+        competition_name_root = os.path.join(competition_root, competition_name)
+        if not os.path.isdir(competition_name_root):
+            continue
+
+        if not _validate_directories(schema, competition_name_root, items, competition_name):
+            success = False
+
+    if not _validate_directories(schema, generic_root, items):
+        success = False
 
     if not success:
         exit(1)
+
+    if contact_api:
+        url = urllib.parse.urljoin(api_base_url, "/v1/quickstarters/~")
+
+        response = requests.post(
+            url,
+            headers={
+                "Authorization": f"API-Key {api_key}"
+            },
+            json={
+                "quickstarters": items
+            }
+        )
+
+        if response.status_code != 201:
+            print(f"api: {response}")
+            print()
+            print_tab(json.dumps(response.json(), indent=4))
+            print()
+            exit(1)
+
+        print(f"api: {response.json()}")
 
 
 if __name__ == '__main__':
