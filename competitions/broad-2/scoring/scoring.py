6f64457b392898ad18da5b6301815b1eb33f6e97
Caceresenzo <caceresenzo1502@gmail.com>
2024-12-10 19:06:22 +0100

    feat(broad-2/scoring): improve error messages

--- competitions/broad-2/scoring/scoring.py
+++ competitions/broad-2/scoring/scoring.py
@@ -2,9 +2,9 @@ import os
 import typing
 
 import crunch
+import crunch.custom
 import crunch.utils
 import pandas
-import spatialdata
 
 
 class ParticipantVisibleError(Exception):
@@ -21,24 +21,33 @@ def check(
     target_names: typing.List[str],
     phase_type: crunch.api.PhaseType
 ):
+    if "ALL" in target_names:
+        target_names.remove("ALL")
+
     with tracer.log("Loading gene list"):
         gene_csv = pandas.read_csv(f'{data_directory_path}/Crunch2_gene_list.csv')
         gene_names = set(gene_csv['gene_symbols'])
 
     with tracer.log("Check for required columns"):
-        difference = set(prediction.columns) ^ ({'sample'} | gene_names)
+        difference = crunch.custom.utils.delta_message(
+            {'sample'} | gene_names,
+            set(prediction.columns),
+        )
 
         if difference:
-            raise ParticipantVisibleError(f"Missing or extra columns: {', '.join(difference)}")
+            raise ParticipantVisibleError(f"Columns do not match: {difference}")
 
     with tracer.log("Check for missing samples"):
-        difference = set(prediction['sample'].unique()) ^ set(target_names)
+        difference = crunch.custom.utils.delta_message(
+            set(target_names),
+            set(prediction['sample'].unique()),
+        )
 
         if difference:
-            raise ParticipantVisibleError(f"Missing or extra samples: {', '.join(difference)}")
+            raise ParticipantVisibleError(f"Samples do not match: {difference}")
 
     for target_name in tracer.loop(target_names, "Checking target -> {value}"):
-        with tracer.log(f"Filter prediction at target -> {target_name}"):
+        with tracer.log(f"Filter prediction at target"):
             prediction_slice = prediction[prediction['sample'] == target_name]
             prediction_slice = prediction_slice.drop(columns=['sample'])
 
@@ -49,28 +58,31 @@ def check(
 
         with tracer.log("Check for NaN values in predictions"):
             if prediction_slice.isnull().values.any():
-                raise ParticipantVisibleError("Predictions contain NaN values, which are not allowed.")
+                raise ParticipantVisibleError(f"Found NaN values for target `{target_name}`")
 
         with tracer.log("Check that all cell IDs are present in predictions"):
-            missing = set(prediction_slice.index) - cell_ids
+            difference = crunch.custom.utils.delta_message(
+                cell_ids,
+                set(prediction_slice.index),
+            )
 
-            if missing:
-                raise ParticipantVisibleError(f"The following cell IDs are missing in predictions: {', '.join(list(map(str, missing))[-10:])}.")
+            if difference:
+                raise ParticipantVisibleError(f"Cell IDs do not match for target `{target_name}`: {difference}")
 
         with tracer.log("Check data types in the 'prediction' values"):
             if not pandas.api.types.is_numeric_dtype(prediction_slice.values):
-                raise ParticipantVisibleError("The 'prediction' values should only contain numeric values.")  #
+                raise ParticipantVisibleError(f"Found non-numeric values for target `{target_name}`")
 
         with tracer.log("Ensure all prediction values are positive"):
             if (prediction_slice.values < 0).any():
-                raise ParticipantVisibleError("Prediction values should be positive.")
+                raise ParticipantVisibleError(f"Found negative values for target `{target_name}`")
 
         with tracer.log("Verify the size of predictions matches expectations"):
             expected = len(cell_ids) * len(gene_names)
             got = prediction_slice.size
 
             if expected != got:
-                raise ParticipantVisibleError(f"Predictions should have {expected} values but has {got}.")
+                raise ParticipantVisibleError(f"Row count for target `{target_name}` should be {expected} ({len(cell_ids)} cell ids * {len(gene_names)} gene names), but got {got}")
 
 
 def score(
@@ -80,7 +92,7 @@ def score(
     target_and_metrics: typing.List[typing.Tuple[crunch.api.Target, typing.List[crunch.api.Metric]]],
 ):
     """TODO"""
-    pass
+    raise NotImplementedError()
 
 
 def _read_zarr(
@@ -88,6 +100,9 @@ def _read_zarr(
         target_name: str
 ):
     zar_data = os.path.join(data_directory_path, f"{target_name}.zarr")
+
+    with tracer.log("Importing spatialdata"):
+        import spatialdata
 
     with tracer.log("Read the Zarr data"):
         sdata = spatialdata.read_zarr(zar_data, selection=("tables",))
