0a46d515c7f47af2348489602101a9d81b3528c1
Caceresenzo <caceresenzo1502@gmail.com>
2025-02-07 17:22:59 +0100

    feat(broad-2/leaderboard): add custom rank

---
+++ competitions/broad-2/scoring/leaderboard.py
@@ -0,0 +1,53 @@
+import typing
+
+import crunch
+import crunch.custom
+import pandas
+import scipy.stats
+
+
+def rank(
+    target_and_metrics: typing.List[typing.Tuple[crunch.api.Target, typing.List[crunch.api.Metric]]],
+    projects: typing.List[crunch.custom.RankableProject],
+):
+    _, cell_spearman_metric = _find_metric_by_name(target_and_metrics, "ALL", "cell-spearman")
+    _, gene_spearman_metric = _find_metric_by_name(target_and_metrics, "ALL", "gene-spearman")
+
+    cell_spearman_column_name = f"metric:{cell_spearman_metric.name}"
+    gene_spearman_column_name = f"metric:{gene_spearman_metric.name}"
+
+    dataframe = pandas.DataFrame((
+        {
+            "project_id": project.id,
+            cell_spearman_column_name: project.get_metric(cell_spearman_metric.id).score,
+            gene_spearman_column_name: project.get_metric(gene_spearman_metric.id).score,
+        }
+        for project in projects
+    ))
+
+    dataframe["final_score"] = scipy.stats.rankdata(
+        scipy.stats.rankdata(-dataframe[cell_spearman_column_name])
+        + scipy.stats.rankdata(-dataframe[gene_spearman_column_name])
+    )
+
+    dataframe.sort_values(by='final_score', inplace=True)
+
+    return list(map(int, dataframe["project_id"]))
+
+
+def _find_metric_by_name(
+    target_and_metrics: typing.List[typing.Tuple[crunch.api.Target, typing.List[crunch.api.Metric]]],
+    target_name: str,
+    metric_name: str,
+) -> crunch.api.Metric:
+    for target, metrics in target_and_metrics:
+        if target.name != target_name:
+            continue
+
+        for metric in metrics:
+            if metric.name != metric_name:
+                continue
+
+            return target, metric
+
+    return None, None
