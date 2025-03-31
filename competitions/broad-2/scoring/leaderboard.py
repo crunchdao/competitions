ff62582c7e43beb69ce54e195da2beb89e392029
Enzo Caceres <caceresenzo1502@gmail.com>
2025-03-31 15:01:08 +0200

    feat(broad-2/leaderboard): add compare (#40)

--- competitions/broad-2/scoring/leaderboard.py
+++ competitions/broad-2/scoring/leaderboard.py
@@ -1,10 +1,106 @@
 import typing
+import os
 
 import crunch
 import crunch.custom
 import numpy
 import pandas
+import glob
 import scipy.stats
+
+tracer = crunch.utils.Tracer()
+
+
+def compare(
+    targets: typing.List[crunch.api.Target],
+    predictions: typing.Dict[int, pandas.DataFrame],
+    combinations: typing.List[typing.Tuple[int, int]],
+    data_directory_path: str,
+):
+    all_target = _find_target_by_name(targets, "ALL")
+
+    # Avoid hardcoding sample names
+    validation_file_path_prefix = os.path.join(data_directory_path, "validation-")
+    sample_name_to_path = {
+        file_path[len(validation_file_path_prefix):-4]: file_path
+        for file_path in glob.glob(f"{validation_file_path_prefix}*.csv")
+    }
+
+    gene_names_per_sample = {
+        sample_name: set(
+            pandas.read_csv(
+                file_path,
+                index_col="cell_id"
+            ).columns
+        )
+        for sample_name, file_path in sample_name_to_path.items()
+    }
+
+    predictions_by_sample: typing.Dict[int, typing.Dict[str, pandas.DataFrame]] = {}
+
+    for prediction_id, dataframe in tracer.loop(list(predictions.items()), lambda entry: f"Preparing Prediction #{entry[0]}"):
+        prediction_by_sample = {}
+
+        for sample_name, group in tracer.loop(dataframe.groupby("sample"), lambda entry: f"Grouped by {entry[0]}"):
+            with tracer.log("Dropping unused gene columns"):
+                useless_columns = set(group.columns) - gene_names_per_sample[sample_name]
+
+                group.drop(columns=useless_columns, inplace=True)
+
+            with tracer.log("Melting the dataframe"):
+                group = group.melt(
+                    var_name="gene",
+                    value_name="prediction",
+                    ignore_index=False,
+                )
+
+            with tracer.log("Setting the index"):
+                group.index.name = "cell_id"
+                group.set_index([group.index, "gene"], inplace=True)
+
+            with tracer.log("Sorting the index"):
+                group.sort_index(inplace=True)
+
+            prediction_by_sample[sample_name] = group["prediction"]
+
+        predictions_by_sample[prediction_id] = prediction_by_sample
+        del predictions[prediction_id]
+
+    similarities: typing.List[crunch.custom.ComparedSimilarity] = []
+    for left_id, right_id in tracer.loop(combinations, lambda combination: f"Using Combination {combination[0]} <-> {combination[1]}"):
+        left_prediction_by_sample = predictions_by_sample[left_id]
+        right_prediction_by_sample = predictions_by_sample[right_id]
+
+        values = []
+        for target in tracer.loop(targets, lambda target: f"Correlating {target.name}"):
+            if target.virtual:
+                continue
+
+            value = left_prediction_by_sample[target.name].corr(right_prediction_by_sample[target.name], method="spearman")
+            values.append(value)
+
+        value = numpy.nanmean(values)
+
+        print(f"similarity - left_id={left_id} right_id={right_id} value={value}")
+        similarities.append(crunch.custom.ComparedSimilarity(
+            left_id,
+            right_id,
+            all_target.id,
+            value,
+        ))
+
+    return similarities
+
+
+def _find_target_by_name(
+    targets: typing.List[crunch.api.Target],
+    target_name: str,
+) -> crunch.api.Metric:
+    for target in targets:
+        if target.name == target_name:
+            return target
+
+    raise ValueError(f"no target found with name=`{target_name}`")
 
 
 def rank(
