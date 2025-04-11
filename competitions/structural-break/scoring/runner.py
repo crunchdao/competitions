a1206393c65726231583eedb986e93e473ffb4b4
Caceresenzo <caceresenzo1502@gmail.com>
2025-04-11 18:40:25 +0200

    fix(structural-break/runner): load x_test like in infer

--- competitions/structural-break/scoring/runner.py
+++ competitions/structural-break/scoring/runner.py
@@ -13,9 +13,8 @@ if typing.TYPE_CHECKING:
 def load_data(
     data_directory_path: str,
 ):
-    x_train = pandas.read_parquet(os.path.join(data_directory_path, "X_train.parquet"))
-    y_train = pandas.read_parquet(os.path.join(data_directory_path, "y_train.parquet"))["structural_breakpoint"]
-    x_test = pandas.read_parquet(os.path.join(data_directory_path, "X_test.reduced.parquet"))
+    x_train, y_train = _load_train(data_directory_path)
+    x_test, _ = _load_x_test(data_directory_path, True)
 
     return x_train, y_train, x_test
 
@@ -107,3 +106,26 @@ def execute(
         "train": train,
         "infer": infer,
     }
+
+
+def _load_train(data_directory_path: str):
+    x_train = pandas.read_parquet(os.path.join(data_directory_path, "X_train.parquet"))
+    y_train = pandas.read_parquet(os.path.join(data_directory_path, "y_train.parquet"))["structural_breakpoint"]
+
+    return x_train, y_train
+
+
+def _load_x_test(data_directory_path: str, reduced: bool):
+    x_test_name = "X_test.reduced.parquet" if reduced else "X_test.parquet"
+    x_test = pandas.read_parquet(os.path.join(data_directory_path, x_test_name))
+
+    datasets, dataset_ids = [], []
+    for id, dataset in x_test.groupby(x_test.index.get_level_values("id")):
+        dataset.name = id
+
+        datasets.append(dataset)
+        dataset_ids.append(id)
+
+    del x_test
+
+    return datasets, dataset_ids
