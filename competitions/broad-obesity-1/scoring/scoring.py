837048a4a0e1426000b2a7e5f9b7083fb3efe168
Caceresenzo <caceresenzo1502@gmail.com>
2025-11-27 22:42:30 +0100

    feat(broad-obesity-1/scoring): add check function (not complete)

--- competitions/broad-obesity-1/scoring/scoring.py
+++ competitions/broad-obesity-1/scoring/scoring.py
@@ -8,6 +8,7 @@ import pandas
 import scanpy
 import scipy.stats
 from crunch.scoring import ScoredMetric
+from crunch.unstructured.utils import delta_message
 from numpy.typing import NDArray
 
 PREDICTION_FILE_NAME = "prediction.h5ad"
@@ -26,7 +27,71 @@ def check(
     prediction_directory_path: str,
     data_directory_path: str,
 ):
-    pass
+    with tracer.log("Ensure files"):
+        difference = delta_message(
+            {
+                PREDICTION_FILE_NAME,
+                PROGRAM_PROPORTION_FILE_NAME,
+            },
+            set(os.listdir(prediction_directory_path)),
+        )
+
+        if difference:
+            raise ParticipantVisibleError(f"Prediction files are not valid: {difference}")
+
+    with tracer.log("Load prediction: prediction"):
+        prediction = scanpy.read_h5ad(os.path.join(prediction_directory_path, PREDICTION_FILE_NAME))
+
+    with tracer.log("Validating .var_names"):
+        pass  # TODO Validate var_names
+
+    with tracer.log("Validating predict_program_proportion"):
+        data_columns = ["adipo", "pre_adipo", "lipo", "other"]
+
+        with tracer.log("Load prediction"):
+            proportions = pandas.read_csv(os.path.join(prediction_directory_path, PROGRAM_PROPORTION_FILE_NAME))
+
+        with tracer.log("Load expected genes"):
+            expected_genes = _load_predict_perturbations(data_directory_path)
+
+        with tracer.log("Check for required columns"):
+            difference = delta_message(
+                {"gene"} | set(data_columns),
+                set(proportions.columns),
+            )
+
+            if difference:
+                raise ParticipantVisibleError(f"predict_program_proportion's columns do not match: {difference}")
+
+        with tracer.log("Check for rows"):
+            expected_genes_count = len(expected_genes)
+            got_rows = len(proportions)
+
+            if expected_genes_count != got_rows:
+                raise ParticipantVisibleError(f"predict_program_proportion: Row count is incorrect, expected {expected_genes_count} but got {got_rows}")
+
+        with tracer.log("Check for NaN values"):
+            if proportions.isnull().values.any():
+                raise ParticipantVisibleError(f"predict_program_proportion: Found NaN value(s)")
+
+        with tracer.log("Check for infinity values"):
+            proportions = proportions.replace([-numpy.inf, numpy.inf], numpy.nan)
+
+            if proportions.isnull().values.any():
+                raise ParticipantVisibleError(f"predict_program_proportion: Found Inf value(s)")
+
+        with tracer.log("Check for genes"):
+            difference = delta_message(
+                set(expected_genes),
+                set(proportions["gene"].unique()),
+            )
+
+            if difference:
+                raise ParticipantVisibleError(f"predict_program_proportion: Genes do not match: {difference}")
+
+        for column_name in tracer.loop(data_columns, lambda x: f"Check data types in the '{x}' values"):
+            if not pandas.api.types.is_numeric_dtype(proportions[column_name].values):
+                raise ParticipantVisibleError(f"predict_program_proportion: Found non-numeric values for column `{column_name}`")
 
 
 def score(
@@ -43,14 +108,14 @@ def score(
         gtruth_adata = scanpy.read_h5ad(os.path.join(data_directory_path, "150_adata_with_labels_valid.h5ad"))
 
     with tracer.log("Load ground truth: predict_program_proportion"):
-        gtruth_proportion = pandas.read_csv(os.path.join(data_directory_path, "150_adata_with_labels_state_prop_valid.csv"))
+        ground_truth_proportion = pandas.read_csv(os.path.join(data_directory_path, "150_adata_with_labels_state_prop_valid.csv"))
 
     with tracer.log("Load prediction: prediction"):
-        pred_adata = scanpy.read_h5ad(os.path.join(prediction_directory_path, PREDICTION_FILE_NAME))
+        prediction_adata = scanpy.read_h5ad(os.path.join(prediction_directory_path, PREDICTION_FILE_NAME))
 
         with tracer.log("Convert to array"):
-            gtruth_X = _as_numpy_array(gtruth_adata.X)
-            pred_X = _as_numpy_array(pred_adata.X)
+            ground_truth_X = _as_numpy_array(gtruth_adata.X)
+            prediction_X = _as_numpy_array(prediction_adata.X)
 
     with tracer.log("Load prediction: predict_program_proportion"):
         pred_proportion = pandas.read_csv(os.path.join(prediction_directory_path, PROGRAM_PROPORTION_FILE_NAME))
@@ -59,27 +124,27 @@ def score(
         perturbed_centroid = gtruth_adata.uns["perturbed_centroid_train"]
 
     del gtruth_adata
-    del pred_adata
+    del prediction_adata
 
     with tracer.log("Compute pearson(X)"):
         person_value = _pearson(
-            gtruth_X,
-            pred_X,
+            ground_truth_X,
+            prediction_X,
             perturbed_centroid,
         )
 
     with tracer.log("Compute L1_DISTANCE(X)"):
         l1_distance_value = _l1_distance(
-            gtruth_proportion,
+            ground_truth_proportion,
             pred_proportion,
-            gene_col_name="gene",
+            gene_column_name="gene",
         )
 
     with tracer.log("Compute MMD(X)"):
         # mmd_value = 9999.99
         mmd_value = _mmd(
-            gtruth_X,
-            pred_X,
+            ground_truth_X,
+            prediction_X,
         )
 
     print("person(X): {}".format(person_value))
@@ -100,22 +165,22 @@ def score(
 
 
 def _pearson(
-    gtruth_X: NDArray[numpy.float64],
-    pred_X: NDArray[numpy.float64],
+    ground_truth_X: NDArray[numpy.float64],
+    prediction_X: NDArray[numpy.float64],
     perturbed_centroid: NDArray[numpy.float64],
 ) -> float:
-    gtruth_X_target = gtruth_X.mean(axis=0)
-    pred_X_target = pred_X.mean(axis=0)
+    ground_truth_X_target = ground_truth_X.mean(axis=0)
+    prediction_X_target = prediction_X.mean(axis=0)
 
     return scipy.stats.pearsonr(
-        gtruth_X_target - perturbed_centroid,
-        pred_X_target - perturbed_centroid,
+        ground_truth_X_target - perturbed_centroid,
+        prediction_X_target - perturbed_centroid,
     ).statistic
 
 
 def _mmd(
-    gtruth_X: NDArray[numpy.float64],
-    pred_X: NDArray[numpy.float64],
+    ground_truth_X: NDArray[numpy.float64],
+    prediction_X: NDArray[numpy.float64],
 ) -> float:
     def compute_mmd_batch(X_batch, Y_batch, kernel_mul, kernel_num, fix_sigma, kernel_func):
         num_batch_element = X_batch.shape[0]
@@ -130,8 +195,6 @@ def _mmd(
         return mmd_val, num_batch_element ** 2
 
     def gaussian_kernel(source, target, kernel_mul, kernel_num, fix_sigma):
-        '''
-        '''
         # Getting the L2 distance
         n_samples = int(source.shape[0]) + int(target.shape[0])
         total = numpy.concatenate([source, target], axis=0)
@@ -145,6 +208,7 @@ def _mmd(
             bandwidth = fix_sigma
         else:
             bandwidth = numpy.sum(L2_distance) / (n_samples**2 - n_samples)
+
         # Now we will create the multiple bandwidth list
         bandwidth /= kernel_mul ** (kernel_num // 2)
         bandwidth_list = [bandwidth * (kernel_mul**i) for i in range(kernel_num)]
@@ -156,14 +220,14 @@ def _mmd(
     kernel_num = 5
     fix_sigma = None
 
-    num_source = gtruth_X.shape[0]
+    num_source = ground_truth_X.shape[0]
     num_batches = 30 * 5  # (30 for 20k samples)
     batch_size = num_source // num_batches
 
     results = [
         compute_mmd_batch(
-            gtruth_X[bidx * batch_size:(bidx + 1) * batch_size, :],
-            pred_X[bidx * batch_size:(bidx + 1) * batch_size, :],
+            ground_truth_X[bidx * batch_size:(bidx + 1) * batch_size, :],
+            prediction_X[bidx * batch_size:(bidx + 1) * batch_size, :],
             kernel_mul, kernel_num, fix_sigma, gaussian_kernel
         )
         for bidx in tracer.loop(range(num_batches), lambda bidx: f"Processing batch {bidx} of {num_batches}")
@@ -177,33 +241,30 @@ def _mmd(
 
 
 def _l1_distance(
-    true_state_proportion_df: pandas.DataFrame,
-    pred_state_proprotion_df: pandas.DataFrame,
-    gene_col_name: str,
+    ground_truth: pandas.DataFrame,
+    prediction: pandas.DataFrame,
+    gene_column_name: str,
 ) -> float:
-    # Reading the prediction proportion filename
-    # Going over all the genes that were perturbed in this set
-    unique_perturb_genes = list(true_state_proportion_df[gene_col_name].unique())
+    unique_perturb_genes = list(ground_truth[gene_column_name].unique())
 
     all_l1_loss_list = []
     for gene in unique_perturb_genes:
-        # Slicing the column with this gene
-        true_gene_df = true_state_proportion_df[true_state_proportion_df[gene_col_name] == gene]
-        pred_gene_df = pred_state_proprotion_df[pred_state_proprotion_df[gene_col_name] == gene]
+        ground_truth_at_gene = ground_truth[ground_truth[gene_column_name] == gene]
+        prediction_at_gene = prediction[prediction[gene_column_name] == gene]
 
-        assert true_gene_df.shape[0] == 1 and pred_gene_df.shape[0] == 1, "More than one prediction for state"
+        assert ground_truth_at_gene.shape[0] == 1 and prediction_at_gene.shape[0] == 1, "More than one prediction for state"
 
         # Getting the L1 loss for main  pre, adipo and other
         l1_three = (
-            numpy.abs(true_gene_df.iloc[0]["pre_adipo"] - pred_gene_df.iloc[0]["pre_adipo"]) +
-            numpy.abs(true_gene_df.iloc[0]["adipo"] - pred_gene_df.iloc[0]["adipo"]) +
-            numpy.abs(true_gene_df.iloc[0]["other"] - pred_gene_df.iloc[0]["other"])
+            numpy.abs(ground_truth_at_gene.iloc[0]["pre_adipo"] - prediction_at_gene.iloc[0]["pre_adipo"]) +
+            numpy.abs(ground_truth_at_gene.iloc[0]["adipo"] - prediction_at_gene.iloc[0]["adipo"]) +
+            numpy.abs(ground_truth_at_gene.iloc[0]["other"] - prediction_at_gene.iloc[0]["other"])
         )
 
         # Getting the L1 loss for lipo by adipo
         numerical_stab_term = 1e-20
-        pred_lipo_adipo = pred_gene_df.iloc[0]["lipo"] / (pred_gene_df.iloc[0]["adipo"] + numerical_stab_term)
-        true_lipo_adipo = true_gene_df.iloc[0]["lipo"] / (true_gene_df.iloc[0]["adipo"] + numerical_stab_term)
+        pred_lipo_adipo = prediction_at_gene.iloc[0]["lipo"] / (prediction_at_gene.iloc[0]["adipo"] + numerical_stab_term)
+        true_lipo_adipo = ground_truth_at_gene.iloc[0]["lipo"] / (ground_truth_at_gene.iloc[0]["adipo"] + numerical_stab_term)
         l1_lipo_adipo = numpy.abs(true_lipo_adipo - pred_lipo_adipo)
 
         # Getting the average error
@@ -235,3 +296,18 @@ def _find_metric_by_name(
                 return metric
 
     raise ValueError(f"metric {name} not found")
+
+
+def _load_predict_perturbations(
+    data_directory_path: str,
+) -> List[str]:
+    genes = []
+
+    with open(os.path.join(data_directory_path, "predict_perturbations.txt"), "r") as fd:
+        for line in fd.readlines():
+            line = line.strip()
+
+            if line:
+                genes.append(line)
+
+    return genes
