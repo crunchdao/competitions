22d74800e7c3532235535a2597282a1c83274788
Enzo Caceres <caceresenzo1502@gmail.com>
2026-02-24 18:45:40 +0100

    feat(causality-discovery): convert to an unstructured competition (#66)

---
+++ competitions/causality-discovery/scoring/runner.py
@@ -0,0 +1,124 @@
+import os
+from typing import TYPE_CHECKING
+
+import joblib
+import pandas
+from crunch.utils import smart_call
+
+if TYPE_CHECKING:
+    from crunch.runner.unstructured import RunnerContext, RunnerExecutorContext, UserModule
+
+
+PREDICTION_FILE_NAME = "prediction.parquet"
+
+
+def load_data(
+    data_directory_path: str,
+):
+    x_train = joblib.load(os.path.join(data_directory_path, "X_train.pickle"))
+    y_train = joblib.load(os.path.join(data_directory_path, "y_train.pickle"))
+    x_test = joblib.load(os.path.join(data_directory_path, "X_test_reduced.pickle"))
+
+    return (
+        x_train,
+        y_train,
+        x_test,
+    )
+
+
+def run(
+    context: "RunnerContext",
+    data_directory_path: str,
+    prediction_directory_path: str,
+):
+    x_train_file_path = os.path.join(data_directory_path, "X_train.pickle")
+    y_train_file_path = os.path.join(data_directory_path, "y_train.pickle")
+
+    if context.is_local:
+        x_test_file_path = os.path.join(data_directory_path, "X_test_reduced.pickle")
+    else:
+        x_test_file_path = os.path.join(data_directory_path, "X_test.pickle")
+
+    prediction_file_path = os.path.join(prediction_directory_path, PREDICTION_FILE_NAME)
+
+    if context.force_first_train:
+        context.execute(
+            command="train",
+            parameters={
+                "x_train_file_path": x_train_file_path,
+                "y_train_file_path": y_train_file_path,
+            }
+        )
+
+    context.execute(
+        command="infer",
+        parameters={
+            "x_test_file_path": x_test_file_path,
+            "prediction_file_path": prediction_file_path,
+        }
+    )
+
+
+def execute(
+    context: "RunnerExecutorContext",
+    module: "UserModule",
+    data_directory_path: str,
+    model_directory_path: str,
+):
+    default_values = {
+        "data_directory_path": data_directory_path,
+        "model_directory_path": model_directory_path,
+        "id_column_name": "example_id",
+        "moon_column_name": "dataset",
+        "prediction_column_name": "prediction",
+    }
+
+    def train(
+        x_train_file_path: str,
+        y_train_file_path: str,
+    ):
+        x_train = joblib.load(x_train_file_path)
+        y_train = joblib.load(y_train_file_path)
+
+        context.trip_data_fuse()
+
+        train_function = module.get_function("train")
+
+        smart_call(
+            train_function,
+            default_values,
+            {
+                "x_train": x_train,
+                "X_train": x_train,
+                "y_train": y_train,
+            }
+        )
+
+    def infer(
+        x_test_file_path: str,
+        prediction_file_path: str,
+    ):
+        x_test = joblib.load(x_test_file_path)
+
+        context.trip_data_fuse()
+
+        infer_function = module.get_function("infer")
+
+        prediction = smart_call(
+            infer_function,
+            default_values,
+            {
+                "x_test": x_test,
+                "X_test": x_test,
+            }
+        )
+
+        if not isinstance(prediction, pandas.DataFrame):
+            raise ValueError("infer() must return a pandas.DataFrame()")
+
+        prediction.to_parquet(prediction_file_path, index=False)
+
+    return {
+        "train": train,
+        "infer": infer,
+    }
---
+++ competitions/causality-discovery/scoring/scoring.py
@@ -0,0 +1,235 @@
+import os
+from typing import Dict, List, Tuple, Union
+
+import crunch
+import joblib
+import networkx
+import numpy
+import pandas
+from crunch.scoring import ScoredMetric
+from crunch.unstructured.utils import delta_message
+from crunch.utils import Tracer
+from sklearn.metrics import balanced_accuracy_score
+
+PREDICTION_FILE_NAME = "prediction.parquet"
+
+
+class ParticipantVisibleError(Exception):
+    """unstructured exception for errors related to participant visibility."""
+    pass
+
+
+FROM_COLUMN_NAME = "from"
+TO_COLUMN_NAME = "to"
+
+MAPPING = numpy.full(16, -1)
+MAPPING[0b0000] = 1  # 'Independent'
+MAPPING[0b0001] = 2  # 'Cause of X'
+MAPPING[0b0010] = 3  # 'Cause of Y'
+MAPPING[0b0011] = 4  # 'Confounder'
+MAPPING[0b0100] = 5  # 'Consequence of Y'
+MAPPING[0b1000] = 6  # 'Consequence of X'
+MAPPING[0b1010] = 7  # 'Mediator'
+MAPPING[0b1100] = 8  # 'Collider'
+
+
+class BadGraphError(ParticipantVisibleError):
+    pass
+
+
+tracer = Tracer()
+
+
+def check(
+    prediction_directory_path: str,
+    data_directory_path: str,
+):
+    with tracer.log("Validating prediction file presence"):
+        prediction_file_path = os.path.join(prediction_directory_path, PREDICTION_FILE_NAME)
+        if not os.path.exists(prediction_file_path):
+            raise ParticipantVisibleError(f"Prediction file `{PREDICTION_FILE_NAME}` not found in `{prediction_directory_path}`.")
+
+    with tracer.log("Load prediction"):
+        prediction = pandas.read_parquet(prediction_file_path)
+
+    with tracer.log("Load example prediction"):
+        example_prediction = pandas.read_parquet(os.path.join(data_directory_path, "example_prediction.parquet"))
+
+    with tracer.log("Check column names"):
+        difference = delta_message(example_prediction.columns, prediction.columns)
+
+        if difference:
+            raise ParticipantVisibleError(f"Columns do not match: {difference}")
+
+    with tracer.log("Check for NaNs"):
+        if prediction.isna().any().any():
+            raise ParticipantVisibleError("Prediction contains NaNs values.")
+
+    with tracer.log("Check for infinite values"):
+        if prediction.isin([numpy.inf, -numpy.inf]).any().any():
+            raise ParticipantVisibleError("Prediction contains infinite values.")
+
+    with tracer.log("Validate IDs"):
+        difference = delta_message(example_prediction["example_id"], prediction["example_id"])
+
+        if difference:
+            raise ParticipantVisibleError(f"IDs do not match: {difference}")
+
+    with tracer.log("Validate values are either 0 or 1"):
+        if not prediction["prediction"].isin([0, 1]).all().all():
+            raise ParticipantVisibleError("Prediction contains values other than 0 or 1.")
+
+
+def score(
+    prediction_directory_path: str,
+    data_directory_path: str,
+    target_and_metrics: List[Tuple[crunch.api.Target, List[crunch.api.Metric]]],
+):
+    metric = target_and_metrics[0][1][0]
+
+    with tracer.log("Load prediction"):
+        prediction = pandas.read_parquet(os.path.join(prediction_directory_path, PREDICTION_FILE_NAME))
+
+    with tracer.log("Process prediction"):
+        prediction = _process_prediction(prediction)
+
+    with tracer.log("Load y_test"):
+        y_test = joblib.load(os.path.join(data_directory_path, "y_test.pickle"))
+
+    with tracer.log("Process y_test"):
+        y_test = _process_y(y_test)
+
+    with tracer.log("Merge prediction and y_test"):
+        merged = pandas.merge(
+            y_test,
+            prediction,
+            on=[
+                "dataset",
+                "example_id",
+            ]
+        )
+
+    with tracer.log("Compute metric"):
+        score_value = balanced_accuracy_score(
+            y_true=merged["target"],
+            y_pred=merged["prediction"],
+        )
+
+    return {
+        metric.id: ScoredMetric(
+            value=score_value,
+        ),
+    }
+
+
+def get_labels(
+    key: Union[str, int],
+    pivoted: pandas.DataFrame,
+
+    # do not change, local lookups are faster than globals
+    mapping=MAPPING,
+):
+    """
+    Classify the nodes of g as "collider", "confounder", etc., wrt the edge X→Y
+
+    For each node i, we look at the role of i wrt X→Y, ignoring all other nodes.
+    There are 8 possible cases:
+    - Cause of X
+    - Consequence of X
+    - Confounder
+    - Collider
+    - Mediator
+    - Independent
+    - Cause of Y
+    - Consequence of Y
+
+    Caveat:
+    - The notions of "confounder", "collider", etc. only make sense for small, textbook graphs.
+
+    Input:  g: nx.DiGraph object, with an edge X→Y
+    Output: list of tuple, with the edges as keys (excluding 'X' and 'Y'): (dataset_id, node, value)
+    """
+
+    nodes = pivoted.columns.to_list()
+    graph = networkx.from_pandas_adjacency(pivoted, create_using=networkx.DiGraph)
+
+    if 'X' not in nodes:
+        raise BadGraphError(f"X not in nodes for dataset `{key}`")
+
+    if 'Y' not in nodes:
+        raise BadGraphError(f"Y not in nodes for dataset `{key}`")
+
+    if ('X', 'Y') not in graph.edges:
+        raise BadGraphError(f"X and/or Y not in edges for dataset `{key}`")
+
+    if not networkx.is_directed_acyclic_graph(graph):
+        raise BadGraphError(f"not a directed acyclic graph for dataset `{key}`")
+
+    A = pivoted.values
+
+    x_index = nodes.index('X')
+    y_index = nodes.index('Y')
+
+    return [
+        (
+            key,
+            node,
+            mapping[
+                (A[x_index, index] << 3) |
+                (A[y_index, index] << 2) |
+                (A[index, y_index] << 1) |
+                (A[index, x_index])
+            ]
+        )
+        for index, node in enumerate(nodes)
+        if node not in "XY"
+    ]
+
+
+def _process_prediction(
+    prediction: pandas.DataFrame,
+):
+    id_column = prediction["example_id"].str.split('_')
+    prediction["example_id"] = id_column.str[:-2].str.join('_')
+    prediction["from"] = id_column.str[-2]
+    prediction["to"] = id_column.str[-1]
+
+    groups = prediction.groupby("example_id")
+
+    labelss: List[tuple] = []
+    for key, group in groups:
+        pivoted = group.pivot(
+            index="from",
+            columns="to",
+            values="prediction",
+        )
+
+        labels = get_labels(key, pivoted)
+        labelss.extend(labels)
+
+    return pandas.DataFrame(
+        labelss,
+        columns=[
+            "dataset",
+            "example_id",
+            "prediction",
+        ]
+    )
+
+
+def _process_y(
+    y_test: Dict[Union[str, int], pandas.DataFrame],
+):
+    labelss: List[tuple] = []
+    for key, y in y_test.items():
+        labels = get_labels(key, y)
+        labelss.extend(labels)
+
+    return pandas.DataFrame(
+        labelss,
+        columns=[
+            "dataset",
+            "example_id",
+            "target"
+        ]
+    )
