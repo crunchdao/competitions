81404e626c04dbdd69a889267b4b9458abab1d41
Caceresenzo <caceresenzo1502@gmail.com>
2026-06-01 20:39:58 +0200

    feat(structural-break-real-time/runner): use server to provide infer data

--- competitions/structural-break-real-time/scoring/runner.py
+++ competitions/structural-break-real-time/scoring/runner.py
@@ -1,8 +1,10 @@
-import gc
 import os
-from enum import Enum
+from dataclasses import dataclass
+from enum import Enum, IntEnum
+from socket import IPPROTO_TCP, SOCK_STREAM, TCP_NODELAY, socket
+from threading import Thread
 from types import GeneratorType
-from typing import TYPE_CHECKING, Any, Callable, Generator, Iterator, List, Literal, Optional, Union
+from typing import TYPE_CHECKING, Any, Callable, Generator, Iterator, Literal, Optional, Tuple, Union, cast
 
 import numpy
 import pandas
@@ -12,12 +14,71 @@ if TYPE_CHECKING:
     from crunch.runner.unstructured import RunnerContext, RunnerExecutorContext, UserModule
 
 
-if os.name != "nt":
-    register_at_fork = os.register_at_fork
-else:
-    # the Cloud environment isn't on Windows
-    def register_at_fork(*, after_in_child: Optional[Callable] = None, **kwargs):
+@dataclass
+class Box[T]:
+    value: T
+
+
+_NAN_BYTES = numpy.float32(float("nan")).tobytes()
+
+# the Cloud environment isn't on Windows
+if os.name == "nt":
+    from socket import AF_INET
+
+    ServerEndpointType = int
+
+    def os_register_at_fork(*, after_in_child: Optional[Callable] = None, **kwargs):
         pass
+
+    def os_create_server_socket() -> Tuple[socket, ServerEndpointType]:
+        server = socket(AF_INET, SOCK_STREAM)
+        server.bind(("localhost", 0))
+        server.listen(1)
+
+        return server, server.getsockname()[1]
+
+    def os_accept(server: socket):
+        client, addr = server.accept()
+        client.setsockopt(IPPROTO_TCP, TCP_NODELAY, 1)
+        return client, addr
+
+    def os_create_client_socket():
+        return socket(AF_INET, SOCK_STREAM)
+
+    def os_connect(client: socket, server_port: int):
+        client.connect(("localhost", server_port))
+        client.setsockopt(IPPROTO_TCP, TCP_NODELAY, 1)
+
+else:
+    from socket import AF_UNIX
+
+    ServerEndpointType = str
+
+    os_register_at_fork = os.register_at_fork
+
+    def os_create_server_socket() -> Tuple[socket, ServerEndpointType]:
+        from socket import AF_UNIX
+
+        SOCKET_PATH = "/tmp/runner_socket"
+        if os.path.exists(SOCKET_PATH):
+            os.unlink(SOCKET_PATH)
+
+        server = socket(AF_UNIX, SOCK_STREAM)
+        server.bind(SOCKET_PATH)
+        server.listen(1)
+
+        os.chmod(SOCKET_PATH, 0o666)
+
+        return server, SOCKET_PATH
+
+    def os_create_client_socket():
+        return socket(AF_UNIX, SOCK_STREAM)
+
+    def os_connect(client: socket, socket_path: str):
+        client.connect(socket_path)
+
+    def os_accept(server: socket):
+        return server.accept()
 
 
 def load_data(
@@ -39,6 +100,7 @@ def load_data(
 
 def run(
     context: "RunnerContext",
+    data_directory_path: str,
     prediction_directory_path: str,
 ):
     if context.force_first_train:
@@ -46,17 +108,7 @@ def run(
             command="train",
         )
 
-    prediction_parquet_file_path = os.path.join(prediction_directory_path, "prediction.parquet")
-
-    context.execute(
-        command="infer",
-        parameters={
-            "determinism_check": False,
-            "prediction_parquet_file_path": prediction_parquet_file_path,
-        },
-    )
-
-    prediction = pandas.read_parquet(prediction_parquet_file_path)
+    prediction = run_infer_with_server(context, data_directory_path, determinism_check=False)
 
     if context.is_determinism_check_enabled:
         percentage = 0.3
@@ -64,21 +116,13 @@ def run(
 
         context.log(f"checking determinism by executing the inference again with {percentage * 100:.0f}% of the data (tolerance: {tolerance})")
 
-        prediction_determinism_parquet_file_path = os.path.join(prediction_directory_path, "prediction.determinism.parquet")
-
-        context.execute(
-            command="infer",
-            parameters={
-                "determinism_check": percentage,
-                "prediction_parquet_file_path": prediction_determinism_parquet_file_path,
-            }
-        )
-
-        prediction2 = pandas.read_parquet(prediction_determinism_parquet_file_path)
-        os.unlink(prediction_determinism_parquet_file_path)
+        prediction2 = run_infer_with_server(context, data_directory_path, determinism_check=percentage)
 
         is_deterministic = numpy.allclose(prediction.loc[prediction2.index, "prediction"], prediction2["prediction"], atol=tolerance)
         context.report_determinism(is_deterministic)
+
+    prediction_file_path = os.path.join(prediction_directory_path, "prediction.parquet")
+    prediction.to_parquet(prediction_file_path)
 
 
 def execute(
@@ -108,40 +152,31 @@ def execute(
         )
 
     def infer(
-        determinism_check: Union[Literal[False], float],
-        prediction_parquet_file_path: str,
+        determinism_check: Union[Literal[False], float],  # just for logging
+        server_endpoint: ServerEndpointType,
     ):
-        x_test_name = "X_test.reduced.parquet" if context.is_local else "X_test.parquet"
-        x_test = pandas.read_parquet(os.path.join(data_directory_path, x_test_name))
-
-        datasets: List[Iterator[pandas.Series]] = []
-        for _, dataset in x_test.groupby(x_test.index.get_level_values("id")):
-            datasets.append(dataset)
-
-        if determinism_check is not False:
-            determinism_slice = slice(None, int(len(datasets) * determinism_check))
-            datasets = datasets[determinism_slice]
-
-        del x_test
-        gc.collect()
-
         context.trip_data_fuse()
 
-        infer_function = module.get_function("infer")
+        with os_create_client_socket() as client:
+            os_connect(client, server_endpoint)
+            remote = Remote(client)
 
-        prediction = _run_with_double_protection(
-            datasets=datasets,
-            consumer_factory=lambda datasets: smart_call(
-                infer_function,
-                default_values,
-                {
-                    "datasets": datasets,
-                },
-            ),
-            post_processor=_post_process_infer_yield_result,
-        )
+            infer_function = module.get_function("infer")
 
-        prediction.to_parquet(prediction_parquet_file_path)
+            _run_with_double_protection(
+                remote=remote,
+                consumer_factory=lambda datasets: smart_call(
+                    infer_function,
+                    default_values,
+                    {
+                        "datasets": datasets,
+                    },
+                ),
+                post_processor=_post_process_infer_yield_result,
+            )
+
+        # print(f"[debug] executor total time wasted in sending: {remote.send_wasted_time:.3f}s")
+        # print(f"[debug] executor total time wasted in receiving: {remote.receive_wasted_time:.3f}s")
 
     return {
         "train": train,
@@ -166,6 +201,194 @@ def _load_train(data_directory_path: str):
     return datasets
 
 
+class Remote:
+
+    def __init__(self, client: socket):
+        self._client = client
+
+        self._send_wasted_time_ns = 0.0
+        self._receive_wasted_time_ns = 0.0
+
+    def receive(self) -> Tuple["RemoteCommand", Union[float, int, None]]:
+        # start_time = perf_counter_ns()
+
+        data = self.receive_raw(1 + 4)
+        if not data:
+            raise RemoteError("connection closed by client")
+
+        command, value = RemoteCommand.decode(data)
+
+        # self._receive_wasted_time_ns += (perf_counter_ns() - start_time)
+        return command, value
+
+    def receive_raw(self, length: int) -> bytes:
+        # start_time = perf_counter_ns()
+
+        data = bytearray()
+        while len(data) < length:
+            packet = self._client.recv(length - len(data))
+            if not packet:
+                raise RemoteError("connection closed by client")
+            data.extend(packet)
+
+        # self._receive_wasted_time_ns += (perf_counter_ns() - start_time)
+        return bytes(data)
+
+    def send(self, command: "RemoteCommand", value: Union[float, int] = float("nan")):
+        # start_time = perf_counter_ns()
+        data = command.encode(value)
+
+        self._client.send(data)
+        # self._send_wasted_time_ns += (perf_counter_ns() - start_time)
+
+    def send_raw(self, data: bytes):
+        # start_time = perf_counter_ns()
+        self._client.send(data)
+        # self._send_wasted_time_ns += (perf_counter_ns() - start_time)
+
+    def expect(self, expected: "RemoteCommand") -> Union[float, int, None]:
+        command, value = self.receive()
+
+        if command != expected:
+            raise RemoteError(f"expected command {expected.name} but got {command.name}")
+
+        return value
+
+    def expect2(self, expected: "RemoteCommand", permitted: "RemoteCommand") -> Tuple["RemoteCommand", Union[float, int, None]]:
+        command, value = self.receive()
+
+        if command != expected and command != permitted:
+            raise RemoteError(f"expected command {expected.name} or {permitted.name} but got {command.name}")
+
+        return command, value
+
+    # @property
+    # def send_wasted_time(self) -> float:
+    #     return self._send_wasted_time_ns / 1e9
+
+    # @property
+    # def receive_wasted_time(self) -> float:
+    #     return self._receive_wasted_time_ns / 1e9
+
+
+class RemoteError(RuntimeError):
+    pass
+
+
+class RemoteCommand(IntEnum):
+    READY = 1
+    NEW_TIMESERIES = 2
+    HISTORICAL_DATA = 4
+    ONLINE_POINT = 5
+    END_TIMESERIES = 6
+    END = 7
+    NEW_PREDICTION = 100
+
+    def encode(self, value: Union[float, int]) -> bytes:
+        data = bytearray(1 + 4)
+        data[0] = self.value
+
+        if self == RemoteCommand.HISTORICAL_DATA:
+            data[1:5] = numpy.int32(value).tobytes()
+        elif self == RemoteCommand.ONLINE_POINT or self == RemoteCommand.NEW_PREDICTION:
+            data[1:5] = numpy.float32(value).tobytes()
+        else:
+            data[1:5] = _NAN_BYTES
+
+        return bytes(data)
+
+    @staticmethod
+    def decode(data: bytes) -> Tuple["RemoteCommand", Optional[float]]:
+        command = RemoteCommand(data[0])
+
+        if command == RemoteCommand.HISTORICAL_DATA:
+            value = numpy.frombuffer(data[1:5], dtype=numpy.int32)[0]
+        elif command == RemoteCommand.ONLINE_POINT or command == RemoteCommand.NEW_PREDICTION:
+            value = numpy.frombuffer(data[1:5], dtype=numpy.float32)[0]
+        else:
+            value = None
+
+        return command, value
+
+
+def run_infer_with_server(
+    context: "RunnerContext",
+    data_directory_path: str,
+    determinism_check: Union[Literal[False], float],
+) -> pandas.DataFrame:
+    x_test_name = "X_test.reduced.parquet" if context.is_local else "X_test.parquet"
+    x_test = pandas.read_parquet(os.path.join(data_directory_path, x_test_name))
+
+    if determinism_check is not False:
+        ids = sorted(set(x_test.index.get_level_values("id")))
+        determinism_slice = slice(None, int(len(ids) * determinism_check))
+        x_test = x_test.loc[x_test.index.get_level_values("id").isin(ids[determinism_slice])]
+
+    server, endpoint = os_create_server_socket()
+
+    predicted_values = []
+    prediction_index = []
+
+    def run():
+        client, _ = os_accept(server)
+        with client:
+            server.close()
+
+            remote = Remote(client)
+
+            remote.expect(RemoteCommand.READY)
+
+            for _, dataset in x_test.groupby(x_test.index.get_level_values("id")):
+                remote.send(RemoteCommand.NEW_TIMESERIES)
+
+                historical, online = _split_periods(dataset, online_values=False)
+                remote.send(RemoteCommand.HISTORICAL_DATA, len(historical))
+                remote.send_raw(historical.astype(numpy.float32).tobytes())  # pyright: ignore[reportAttributeAccessIssue]
+
+                for index, value in online.items():  # pyright: ignore[reportAttributeAccessIssue]
+                    remote.send(RemoteCommand.ONLINE_POINT, value)
+
+                    prediction = remote.expect(RemoteCommand.NEW_PREDICTION)
+                    predicted_values.append(prediction)
+                    prediction_index.append(index)
+
+                remote.send(RemoteCommand.END_TIMESERIES)
+
+            remote.send(RemoteCommand.END)
+
+        # context.log(f"[debug] runner total time wasted in sending: {remote.send_wasted_time:.3f}s")
+        # context.log(f"[debug] runner total time wasted in receiving: {remote.receive_wasted_time:.3f}s")
+
+    def run_with_catch():
+        try:
+            run()
+        except Exception as error:
+            context.log(f"error in data thread: {error}")
+
+    thread = Thread(target=run_with_catch, daemon=True)
+    thread.start()
+
+    context.execute(
+        command="infer",
+        parameters={
+            "determinism_check": determinism_check,
+            "server_endpoint": endpoint,
+        }
+    )
+
+    thread.join()
+
+    return pandas.DataFrame(
+        data={
+            "prediction": predicted_values,
+        },
+        index=pandas.MultiIndex.from_tuples(
+            prediction_index,
+            names=["id", "time"],
+        ),
+    )
+
+
 class ProtocolError(RuntimeError):
     pass
 
@@ -176,7 +399,7 @@ class NestedForkError(RuntimeError):
 
 def _run_with_double_protection(
     *,
-    datasets: List[pandas.DataFrame],
+    remote: "Remote",
     consumer_factory: Callable[[Iterator], Generator],
     post_processor: Callable[[Any], Any],
 ):
@@ -217,25 +440,22 @@ def _run_with_double_protection(
             yield next_value
 
     # 3: provide the values
-    def online_stream(online: pandas.Series):
+    def online_stream(next_online_value: "Box"):
         nonlocal state
 
-        for value in online:
+        while next_online_value.value is not None:
             # X: ensure not forked
             if state == State.NESTED_FORK:
                 raise NestedForkError("cannot iterate in online stream in a child process after a fork")
 
             # 6: ensure not iterating a dataset
             if state != State.READY_FOR_VALUE:
-                raise ProtocolError("previous value not yield-ed")
+                raise ProtocolError(f"previous value not yield-ed")
 
             # 6.1: mark value as consumed
             state = State.CONSUMED
 
-            yield value
-
-    prediction_index = []
-    prediction_values = []
+            yield next_online_value.value
 
     # X: triggered in the child process after fork, prevent iterations
     def _after_in_child():
@@ -245,7 +465,7 @@ def _run_with_double_protection(
         next_value = None
 
     # X: register security measure
-    register_at_fork(after_in_child=_after_in_child)
+    os_register_at_fork(after_in_child=_after_in_child)
 
     if True:
         # 1: initialize user code
@@ -262,7 +482,14 @@ def _run_with_double_protection(
         if state == State.NESTED_FORK:
             raise NestedForkError(f"cannot do first yield in a child process after a fork ({os.getpid()})")
 
-    for dataset in datasets:
+        # 1.3: send server ready signal
+        remote.send(RemoteCommand.READY)
+
+    while True:
+        command, _ = remote.expect2(RemoteCommand.NEW_TIMESERIES, RemoteCommand.END)
+        if command == RemoteCommand.END:
+            break
+
         # X: ensure not forked
         if state == State.NESTED_FORK:
             raise NestedForkError(f"cannot iterate in datasets in a child process after a fork ({os.getpid()})")
@@ -270,12 +497,24 @@ def _run_with_double_protection(
         # 2: allow reading a dataset next
         state = State.READY_FOR_DATASET
 
-        historical, online = _split_periods(dataset, online_values=False)
+        # 2.1: fetch the historical data
+        historical_length = cast(int, remote.expect(RemoteCommand.HISTORICAL_DATA))
+        historical_data = remote.receive_raw(historical_length * 4)
+        historical = numpy.frombuffer(historical_data, dtype=numpy.float32)
+        del historical_data
 
-        # 2.1: set value for dataset stream
-        next_value = (historical, online_stream(online))
+        # 2.2: prepare the online stream
+        next_online_value: Box[Optional[float]] = Box(None)
+        online_generator = online_stream(next_online_value)
 
-        for index in online.index:
+        # 2.3: prepare the next value of the dataset stream
+        next_value = (historical, online_generator)
+
+        while True:
+            command, next_online_value.value = remote.expect2(RemoteCommand.ONLINE_POINT, RemoteCommand.END_TIMESERIES)
+            if command == RemoteCommand.END_TIMESERIES:
+                break
+
             # 4: re-activate user code
             y = next(user_code, sentinel)
             if y is sentinel:
@@ -294,8 +533,10 @@ def _run_with_double_protection(
             # 7.1: reset state for next value
             state = State.READY_FOR_VALUE
 
-            prediction_index.append(index)
-            prediction_values.append(y)
+            if next_online_value.value is None:
+                break
+
+            remote.send(RemoteCommand.NEW_PREDICTION, y)
 
         # 8: clean up for next dataset
         next_value = None
@@ -305,16 +546,6 @@ def _run_with_double_protection(
         last_check = next(user_code, sentinel)
         if last_check is not sentinel:
             raise ProtocolError("code did not exit after last dataset")
-
-    return pandas.DataFrame(
-        data={
-            "prediction": prediction_values,
-        },
-        index=pandas.MultiIndex.from_tuples(
-            prediction_index,
-            names=["id", "time"],
-        ),
-    )
 
 
 def _split_periods(
